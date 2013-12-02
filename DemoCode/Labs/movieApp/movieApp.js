e34040b077be9d7e18597f48556730bfa7c37b17
K. Scott Allen <scott@odetocode.com>
2013-12-02 06:01:24 -0500

    lab 01

---
+++ DemoCode/Labs/movieApp/movieApp.js
@@ -0,0 +1,18 @@
+﻿var MovieListController = function($scope) {
+
+    $scope.movies =
+    [
+        { title: "Star Wars", length: 90 },
+        { title: "Star Trek", length: 120 },
+        { title: "Star Gates", length: 90 }
+    ];
+
+
+    $scope.increment = function(movie) {
+        movie.length += 1;
+    };
+
+    $scope.decrement = function(index) {
+        $scope.movies[index].length -= 1;
+    };
+};
