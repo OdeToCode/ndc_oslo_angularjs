8eca71da457a53c5dba3a5e87a8f85bb6bfafca5
K. Scott Allen <scott@odetocode.com>
2013-12-03 08:17:47 -0500

    rating directive

--- DemoCode/Labs/movieApp/services/movieService.js
+++ DemoCode/Labs/movieApp/services/movieService.js
@@ -3,19 +3,6 @@
     var movieService = function($http, $q) {
 
         var getAllMovies = function() {
-
-            //var deferred = $q.defer();
-
-            //deferred.resolve({
-            //    data: [
-            //        { "title": "Star Wars", "length": 90 },
-            //        { "title": "Star Trek", "length": 120 },
-            //        { "title": "Starship Troopers", "length": 90 }
-            //    ]
-            //});
-
-            //return deferred.promise;
-
             return $http.get("/movieApp/data/movies.json");
         };
 
