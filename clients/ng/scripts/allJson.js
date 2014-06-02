1ca8e57ea0600699cac02b86ccf05a656daf1508
K. Scott Allen <scott@odetocode.com>
2014-06-02 08:45:50 -0400

    finished views module

---
+++ clients/ng/scripts/allJson.js
@@ -0,0 +1,12 @@
+﻿(function() {
+
+    var allJson = function() {
+        return function (data) {
+            return JSON.stringify(data, null, 2);
+        };
+    };
+
+    var app = angular.module("atTheMovies");
+    app.filter("allJson", allJson);
+
+}());
