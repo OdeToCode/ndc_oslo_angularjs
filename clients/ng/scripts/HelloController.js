bffafc814df6a36338b93b0d92f47e50114f3163
K. Scott Allen <scott@odetocode.com>
2014-06-02 04:00:13 -0400

    complete module 1

---
+++ clients/ng/scripts/HelloController.js
@@ -0,0 +1,24 @@
+﻿var HelloController = function($scope) {
+    
+    $scope.showMessage = function() {
+        $scope.message = "Hello, World!";
+    };
+
+    $scope.saveMessage = function() {
+        $scope.alert = "Saved...";
+    };
+
+};
+
+
+var MoviesController = function($scope) {
+
+    var movies = [
+        { title: "Star Wars", length: 120, released: 1981 },
+        { title: "Top Gun", length: 90, released: 1984 },
+        { title: "Hot Shots", length: 89, released: 1986 }
+    ];  
+
+    $scope.movies = movies;
+
+};
